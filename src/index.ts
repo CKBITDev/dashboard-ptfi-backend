cfcf9e487a658384422edf795106d7a8c25bef97
Muhammad Oktoluqman Fakhrianto <dimas.oktoluqman@gmail.com>
2024-10-09 14:51:55 +0700

    feat: install cors and setup cors .env

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 import routes from "./router";
 import appLogger from "./utils/logger";
@@ -8,6 +9,18 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN?.split(";"),
+    allowedHeaders: [
+      "Content-Type",
+      "Accept",
+      "Authorization",
+      "X-Requested-With",
+    ],
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
